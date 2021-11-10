2956cf8dec40d2973199c3b08fd81aa33c2a5f5d
Simon Miller <mail@simonmiller.se>
2021-11-10 13:04:43 +0100

    rock-paper-scissors funtionality

---
+++ simon/index.js
@@ -0,0 +1,88 @@
+let userChoice = document.querySelector('.input');
+console.log(userChoice);
+let play = document.querySelector('.btn');
+let choiceOutput = document.querySelector('.userOutput');
+let computerOutput = document.querySelector('.compOutput');
+let reload = document.querySelector('.reload');
+let winner = document.querySelector('.winner')
+
+
+const getUserChoice = (userChoice) => {
+    userChoice = userChoice.toLowerCase();   
+}
+
+
+const getComputerChoice = () => {
+    const num = Math.floor(Math.random() * 3);
+    switch (num) {
+      case 0:
+        return 'rock';
+      case 1:
+        return 'paper';
+      case 2:
+        return 'scissors';
+    }
+  };
+
+  computerChoice = getComputerChoice();
+
+  console.log(computerChoice);
+  
+
+  const getWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+      return 'it\'s oficially a tie!';
+    }
+  
+    if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+        return 'Sorry, you lost.';
+      } else {
+        return 'Congratulations, you won!';
+      }
+    }
+  
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+          return 'Sorry. Better luck next time.';
+        } else {
+          return 'Congratulations, you won!';
+        }
+      }
+  
+    if (userChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+        return 'Sorry, computer won.';
+      } else {
+        return 'Congratulations, you won!';
+      }
+    }
+  
+    if (userChoice === 'bomb') {
+      return 'Congratulations, You won!'
+    }
+  };
+  
+
+
+play.addEventListener('click', () => {
+    userChoice = userChoice.value;
+    console.log(userChoice);
+
+    if (userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors') {
+        choiceOutput.innerHTML = userChoice;
+    } else {
+        choiceOutput.innerHTML = 'Invalid Choice';
+    }
+
+    computerOutput.innerHTML = computerChoice;
+
+    winner.innerHTML = getWinner(userChoice, computerChoice);
+
+    reload.style.display = "block";
+    play.style.display = "none";
+});
+
+reload.addEventListener('click', () => {
+    window.location.reload();
+})
